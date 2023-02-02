7c62ebe405efcdcab96c493030b0100a851812c4
Dustin Van Tate Testa <toast27@gmail.com>
2023-02-02 00:00:30 -0600

    new domain

--- texture_cube.three.js
+++ texture_cube.three.js
@@ -5,28 +5,26 @@ init();
 animate();
 
 function init() {
-
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
     camera.position.z = 1;
 
     scene = new THREE.Scene();
-
-		const ambient = new THREE.AmbientLight( 0xffffff );
+    const ambient = new THREE.AmbientLight( 0xffffff );
     scene.add( ambient );
         
     const loader = new THREE.CubeTextureLoader();
     const texture = loader.load([
-    	'http://me.dvtate.com/imgs/corki.png',
-    	'http://me.dvtate.com/imgs/corki.png',
-    	'http://me.dvtate.com/imgs/corki.png',
+    	'http://ridder.xyz/imgs/corki.png',
+    	'http://ridder.xyz/imgs/corki.png',
+    	'http://ridder.xyz/imgs/corki.png',
       
-    	'http://me.dvtate.com/imgs/corki.png',
-    	'http://me.dvtate.com/imgs/corki.png',
-    	'http://me.dvtate.com/imgs/corki.png',
+    	'http://ridder.xyz/imgs/corki.png',
+    	'http://ridder.xyz/imgs/corki.png',
+    	'http://ridder.xyz/imgs/corki.png',
     ]);
     const geometry = new THREE.BoxGeometry(.4, .4, .4);
     const material = new THREE.MeshLambertMaterial( {
-    	/* color: 0x00ff00, */
+      /* color: 0x00ff00, */
       envMap: texture,  
     });
     cube = new THREE.Mesh( geometry, material );
@@ -35,16 +33,15 @@ function init() {
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-
 }
 
 function animate() {
-
     requestAnimationFrame( animate );
     
     cube.rotation.y += 0.02;
     cube.rotation.z += 0.005;
 
     renderer.render( scene, camera );
-
 }
+
+// TODO add point light that orbits cube
